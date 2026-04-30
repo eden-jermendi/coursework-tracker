69b551d1342f0d1dc56be99fbd38fb581f636cd9
Eden Jermendi <ejermendi@gmail.com>
2026-04-30 14:36:09 +1200

    feat: added remaning DB functions and routes - testing in postman now

--- server/db/coursework.ts
+++ server/db/coursework.ts
@@ -1,8 +1,31 @@
-import { Coursework } from "../../models/coursework"
-import connection from "./connection"
+import { Coursework, CourseworkData } from '../../models/coursework'
+import connection from './connection'
 
 export async function getCoursework(db = connection): Promise<Coursework[]> {
+  return db('coursework').select()
+}
+
+export async function addCoursework(
+  coursework: CourseworkData,
+  db = connection,
+): Promise<Coursework> {
   return db('coursework')
-    .select()
-    .orderBy('id')
+    .insert(coursework)
+    .returning('*')
+    .then((insertedEntries) => insertedEntries[0])
+}
+
+export async function updateCoursework(
+  updatedCoursework: CourseworkData,
+  db = connection,
+): Promise<Coursework> {
+  return db('coursework')
+    .where({ id })
+    .update(updatedCoursework)
+    .returning('*')
+    .then((updatedEntries) => updatedEntries[0])
+}
+
+export async function deleteCoursework(id: number, db = connection) {
+  return db('coursework').where({ id }).delete()
 }
