2814640be473f46449fd4bbf29eff09e72da2109
Eden Jermendi <ejermendi@gmail.com>
2026-05-02 12:59:56 +1200

    added process.env if block for env variables

--- server/server.ts
+++ server/server.ts
@@ -12,20 +12,20 @@ server.use(express.json())
 server.use('/api/v1/coursework', courseworkRoutes)
 server.use('/api/v1/joke', jokeRoutes)
 
-if (process.env.NODE_ENV === 'production') {
-  server.use(express.static(Path.resolve('public')))
-  server.use('/assets', express.static(Path.resolve('./dist/assets')))
-  server.get('*', (req, res) => {
-    res.sendFile(Path.resolve('./dist/index.html'))
-  })
-}
-
 if (process.env.NODE_ENV !== 'production') {
   import('dotenv')
     .then((dotenv) => dotenv.config())
     .catch((err) => {
       console.error('Failed to load dotenv: ', err)
     })
+}
+
+if (process.env.NODE_ENV === 'production') {
+  server.use(express.static(Path.resolve('public')))
+  server.use('/assets', express.static(Path.resolve('./dist/assets')))
+  server.get('*', (req, res) => {
+    res.sendFile(Path.resolve('./dist/index.html'))
+  })
 }
 
 export default server
